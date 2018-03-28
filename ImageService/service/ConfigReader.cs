a3c8acb1e653dbc55c8e4ea0b1e11c9b11ac9298
omer barak <omer.barak@live.biu.ac.il>
2018-03-28 15:10:46 +0300

    doing xml

--- ImageService/service/ConfigReader.cs
+++ ImageService/service/ConfigReader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
 namespace ImageService.service
 {
     class ConfigReader
@@ -12,11 +12,15 @@ namespace ImageService.service
         private string OutputDir;
         private string SourceName;
         private string LogName;
-        private string ThumbnailSize;
+        private int ThumbnailSize;
 
         public ConfigReader()
         {
-            //System.Configuration.ConfigurationSettings.AppSettings["App.config"];
+            this.Handler = ConfigurationManager.AppSettings["Handler"];
+            this.OutputDir = ConfigurationManager.AppSettings["OutputDir"];
+            this.SourceName = ConfigurationManager.AppSettings["SourceName"];
+            this.LogName = ConfigurationManager.AppSettings["LogName"];
+            this.ThumbnailSize = int.Parse(ConfigurationManager.AppSettings["ThumbnailSize"]);
         }
     }
 }
