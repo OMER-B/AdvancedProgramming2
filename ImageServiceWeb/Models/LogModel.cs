264b4b863c1b15c6d0060e5f7a7497b828474e96
Hodaya <ko.hodaya@gmail.com>
2018-06-03 17:50:36 +0300

    log and config recieve data

--- ImageServiceWeb/Models/LogModel.cs
+++ ImageServiceWeb/Models/LogModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using CommunicationTools;
@@ -9,12 +10,48 @@ namespace ImageServiceWeb.Models
     public class LogModel
     {
         private List<TitleAndContent> list;
+        public List<TitleAndContent> List { get { return this.list; } set { this.list = value; } }
+        private bool recievedHistory = false;
 
+        /// <summary>
+        /// Constructor for LogModel.
+        /// </summary>
         public LogModel()
         {
-            list = new List<TitleAndContent>();
-            list.Add(new TitleAndContent("FAIL", "hello"));
+            this.list = new List<TitleAndContent>();
+            TcpClientChannel.Instance.DataRecieved += GetData;
+            TcpClientChannel.Instance.SendMessage(new TACHolder(MessageTypeEnum.LOG_HISTORY, null).ToJson());
+            while (!recievedHistory) { }
         }
 
+        /// <summary>
+        /// The function called by the event of the tcp-channel.
+        /// when a message is recieved check if it is relevant to
+        /// the log model (send log or log history)
+        /// activate the on property changed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="data"></param>
+        public void GetData(object sender, ClientMessage data)
+        {
+            TACHolder tac = MessageParser.ParseJsonToTAC(data.Message);
+            switch (tac.CommandID)
+            {
+                case MessageTypeEnum.SEND_LOG:
+                    foreach (TitleAndContent t in tac.List)
+                    {
+                        this.list.Add(t);
+                    }
+                    break;
+                case MessageTypeEnum.LOG_HISTORY:
+                    recievedHistory = true;
+                    foreach (TitleAndContent t in tac.List)
+                    {
+                        this.list.Add(t);
+                    }
+                    break;
+                default: break;
+            }
+        }
     }
 }
